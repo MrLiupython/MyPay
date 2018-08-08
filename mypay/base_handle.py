5596b301202d0ae847e582bb22f6f0f042596965
MrLiupython <18270881162@163.com>
2018-08-08 23:34:31 +0800

    base struction

---
+++ mypay/base_handle.py
@@ -0,0 +1,3 @@
+"""
+  Here is a part of mypay to handle data, it can filter those datas,which is invalid or sensitive. It don't care the origin of datas, and it save data to middleware after handled.
+"""
---
+++ mypay/base_kind.py
@@ -0,0 +1,3 @@
+"""
+    It get data from middleware.Then it sort the data according to one field of data.Add kind field to data, save it to core data db.
+"""
